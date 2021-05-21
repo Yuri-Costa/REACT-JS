92df2d15ed460e20f9549112a5ccbe400caff367
yuri costa <yuri342costa@gmail.com>
2021-05-20 23:05:18 -0300

    clock1

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+
+function DataFormatada(props) {
+  return <h2> Horário Atual: {props.date.toLocaleTimeString()}</h2>
+}
+class Relogio extends React.Component{
+  constructor(props){
+    super(props)      //DateTime.Now 
+    this.state = {   date : new Date()     }
+  }
+    //nascimento
+  componentDidMount(){
+      this.timerID = setInterval( () => {  this.thick()        },
+      
+      
+      1000)
+      console.log("Eu sou o relogio " + this.timerID)
+  }
+
+    //morte
+  componentWillUnmount(){
+      clearInterval(this.timerID)
+  }
+  thick(){
+   this.setState ({
+     date : new Date()
+   })
+  }
+  render(){
+    return(
+      <div>
+        <h1>Relógio1</h1>
+        <DataFormatada date = {this.state.date} />
+        
+      </div>
+    )
+  }
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Relogio />
       </header>
     </div>
   );
