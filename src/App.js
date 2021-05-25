4ed078283dded20ab3f88d837041ea6000853ec2
Yuri Costa Camilo <34884566840@info.local>
2021-05-25 13:46:07 -0300

    relogio feito

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import './App.css';
+import bem101 from "./ben10.png";
+import bem102 from "./ben10.png";
 
 function DataFormatada(props) {
-  return <h2> Horário Atual: {props.date.toLocaleTimeString()}</h2>
+  return <p>{props.date.toLocaleTimeString()}</p>
 }
 class Relogio extends React.Component{
   constructor(props){
@@ -22,17 +24,30 @@ class Relogio extends React.Component{
   componentWillUnmount(){
       clearInterval(this.timerID)
   }
+  //Função criada para atualizar o valor do relogio e consequentimente a pagina renderizada
   thick(){
-   this.setState ({
-     date : new Date()
-   })
+    //Set traduzido é definir
+    this.setState({
+      date : new Date()
+    });
   }
+
+   PararRelogio(){
+     clearInterval(this.timerID)
+     console.log("RELÓGIO " + this.timerID+" PAROU")
+  }
+
+    VoltarRealogio(){
+      this.timerID = setInterval( () => {  this.thick()},
+      1000)
+    }
+  
   render(){
     return(
       <div>
-        <h1>Relógio1</h1>
-        <DataFormatada date = {this.state.date} />
-        
+        <DataFormatada date={this.state.date}/>
+        <button class="btn1" onClick={() => this. PararRelogio()}> <h3>STOP!</h3></button>    
+        <button class="btn2" onClick={() => this. VoltarRealogio()}><h3>START!</h3></button>
       </div>
     )
   }
@@ -42,9 +57,22 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Relogio />
+    
+        <div className="div123">
+        <li className ="relogios">
+          <img src = {bem101} className = "ben10"></img>
+          <img src = {bem102} className = "ben10-1"></img>
+        </li>
+        <h3 class= "clock1"> <Relogio /></h3>
+        <h3 class= "clock2"> <Relogio /></h3>
+        </div>
+
+
+
       </header>
+      <style>{}</style>
     </div>
+    
   );
 }
 
